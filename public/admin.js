41c80a48c983fd18a7dd472c6e89e847e3b75b7c
Darius <dariusmmcclendon@gmail.com>
2022-01-16 14:01:11 -0500

    Added rendering javascript, no idea how to create update method

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,32 @@
 
 // Your Code Here
+async function admin(){
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderAdminPage)
+
+    function updateQuantity(ID, newQuantity){
+        for(let i = 0; i < books.length ; i++){
+            if(books[i].id == ID){
+                
+            }
+        }
+    }
+}
+function renderAdminPage(book){
+    let list = document.querySelector('#book-list')
+    let listItem = document.createElement('li')
+    listItem.innerHTML = `
+        <form>
+            <p>${book.title}<p>
+            <input type="text" id="${book.id}-quantity" value="${book.quantity}">
+            <input type="submit" value="Save">
+        </form>
+    `
+    list.append(listItem)
+}
+
+
+
+admin()
