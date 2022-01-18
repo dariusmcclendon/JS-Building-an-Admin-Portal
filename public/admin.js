18be41723f00e0802261a51cefa0d3c12d0d3d37
Darius <dariusmmcclendon@gmail.com>
2022-01-18 10:57:02 -0500

    Added JS for save button, removed method using .innerhtml

--- public/admin.js
+++ public/admin.js
@@ -1,32 +1,42 @@
 
 // Your Code Here
 async function admin(){
+    //get list of books from server
     let response = await fetch('http://localhost:3001/listBooks')
+    //convert to object
     let books = await response.json()
 
-    books.forEach(renderAdminPage)
+    //iterate through books
+    books.forEach(function(book){
+        //generating elements
+        let list = document.querySelector('#book-list')
+        let listItem = document.createElement('li')
+        listItem.textContent = book.title
+        list.append(listItem)
+        let input = document.createElement("input")
+        input.type = "text"
+        input.value = book.quantity
+        listItem.append(input)
+        let saveButton = document.createElement("button")
+        saveButton.textContent = "Save"
+        saveButton.addEventListener("click", async function(){
+            //fetching updatebook function from server
+            await fetch('http://localhost:3001/updatebook',{
+                //goobeldy gook tbh
+                method: "PATCH",
+                headers: {
+                    "Content-Type" : "application/json"
+                },//end header block
+                body : JSON.stringify({
+                    "id" : book.id,
+                    "quantity" : input.value
+                })//end body block
+            })//end fetch
+        })//end event listener
+listItem.append(saveButton)
+    })//end forEach
 
-    function updateQuantity(ID, newQuantity){
-        for(let i = 0; i < books.length ; i++){
-            if(books[i].id == ID){
-                
-            }
-        }
-    }
+
+
 }
-function renderAdminPage(book){
-    let list = document.querySelector('#book-list')
-    let listItem = document.createElement('li')
-    listItem.innerHTML = `
-        <form>
-            <p>${book.title}<p>
-            <input type="text" id="${book.id}-quantity" value="${book.quantity}">
-            <input type="submit" value="Save">
-        </form>
-    `
-    list.append(listItem)
-}
-
-
-
 admin()
